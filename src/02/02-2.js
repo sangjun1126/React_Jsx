ea62f6990f63ba3dd8b4ae5c0a5a7ab7b4ec9827
sangjun1126 <jeongsangjun1234@gmail.com>
2024-01-15 20:31:37 +0900

    first commit

---
+++ src/02/02-2.js
@@ -0,0 +1,23 @@
+var string1 = '안녕하세요';
+var string2 = '반갑습니다';
+var greeting = string1 + ' ' + string2;
+var product = { name: '도서', price: '4200원' };
+var message = '제품' + product.name + '의 가격은' + product.price + '입니다';
+var multiLine = '문자열1\n문자열2';
+var value1 = 1;
+var value2 = 2;
+var boolValue = false;
+var operator1 = '곱셉값은 ' + value1 * value2 + '입니다';
+var operator2 = '불리언값은 ' + (boolValue ? '참' : '거짓') + '입니다';
+
+console.log(string1);
+console.log(string2);
+console.log(greeting);
+console.log(product);
+console.log(message);
+console.log(multiLine);
+console.log(value1);
+console.log(value2);
+console.log(boolValue);
+console.log(operator1);
+console.log(operator2);
